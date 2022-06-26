2cd5d69dd984cd1782b875e8f5c748c59c68c74c
Harry Edgar <harryedgar@hotmail.co.uk>
2022-06-26 09:36:10 +0100

    Complete +-/*= operations

--- main.js
+++ main.js
@@ -2,56 +2,68 @@ const buttons = document.querySelector('#buttonsContainer')
 const screenDisplay = document.querySelector('.screenDisplay')
 const totalDisplay = document.querySelector('.screenDisplayTotal')
 
-
+totalValue = "0"
 screenValue = ""
-totalValue = ""
-intMemory = ""
-operatorMemory = ""
+operatorMemory = "+"
 
 buttons.addEventListener('click', function (e) {
     let input = (e.target.textContent)
-    // console.log(input)
         inputBus(input)
 })
+
 function inputBus (input) {
     if (isNaN(input) === false) { // is a number
-        updateScreen(input)
+        updateScreenNum(input)
     }
     else if (isNaN(input) === true) { // not number
-        if (Boolean(input.match(/-|÷|x|\+/g)) === true) {
+        if (Boolean(input.match(/-|÷|=|x|\+/g)) === true) {
             operatorBus(input)
-        }
-    }
-}
-
+    }}}
 
 function operatorBus (input) {
-    if (intMemory !== "" && operatorMemory !== "" && screenValue !== "") {
-        operatorCalc(input)
-    }
-    operatorMemory = operatorMemory.concat(input)
-    intMemory = screenValue.concat(input)
-    screenValue = ""
-    screenDisplay.textContent = operatorMemory
-}
+    if (Boolean(input.match(/=/g)) === true) {
+        let calc = operators[operatorMemory](totalValue, screenValue)
+        screenDisplay.textContent = calc
+        totalDisplay.textContent = ""
+        totalValue = "0"
+        screenValue = ""
+        operatorMemory = "+"
+        return
+}   else {
+        let calc = operators[operatorMemory](totalValue, screenValue)
+        operatorMemory = input
+        updateTotal(calc)
+        updateScreenOperator(input)
+        return
+    }} 
 
-function updateScreen(input) {
+function updateTotal (calc) {
+    totalValue = calc
+    totalDisplay.textContent = totalValue
+    return
+}
+function updateScreenNum (input) {
     screenValue = screenValue.concat(input)
     screenDisplay.textContent = screenValue
     return
 }
-
-function updateTotal(calc) {
-    totalValue = totalValue.concat(calc)
-    totalDisplay.textContent = totalValue
+function updateScreenOperator (input) {
+    operatorMemory = input
+    screenDisplay.textContent = operatorMemory
+    screenValue = ""
     return
 }
 
-function operatorCalc(input) {
-    if (operatorMemory === '+') {
-        let calc = parseInt(intMemory) + parseInt(screenValue)
-        screenValue= ""
-        operatorMemory = ""
-        updateTotal(calc)
-    }
-}
+const operators = {
+    '+': function(a , b) {
+        return parseInt(a) + parseInt(b)
+    },
+    '-': function(a , b) {
+        return parseInt(a) - parseInt(b)
+    },
+    'x': function(a , b) {
+        return parseInt(a) * parseInt(b)
+    },
+    '÷': function(a , b) {
+        return parseInt(a) / parseInt(b)
+    },}
